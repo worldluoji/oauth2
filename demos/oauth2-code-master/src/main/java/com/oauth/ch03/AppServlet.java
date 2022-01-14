327cb74bd6bbd8d9fe396675101920a5b26cc154
worldluoji <qq1132416@outlook.com>
2022-01-14 17:28:13 +0800

    @oauth2 demo@luoji@20210114

---
+++ demos/oauth2-code-master/src/main/java/com/oauth/ch03/AppServlet.java
@@ -0,0 +1,61 @@
+package com.oauth.ch03;
+
+import com.my.util.HttpURLClient;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * **
+ * 使用此类来模拟【第三方软件的Server端】
+ *
+ */
+@WebServlet("/AppServlet-ch03")
+public class AppServlet extends HttpServlet {
+
+    String oauthURl="http://localhost:8081/OauthServlet-ch03";
+    String protectedURl="http://localhost:8081/ProtectedServlet-ch03";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+        //授权码许可流程，DEMO CODE
+
+        String code = request.getParameter("code");
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("code",code);
+        params.put("grant_type","authorization_code");
+        params.put("app_id","APPID_RABBIT");
+        params.put("app_secret","APPSECRET_RABBIT");
+
+        System.out.println("start post code for token ...");
+        String accessToken = HttpURLClient.doPost(oauthURl,HttpURLClient.mapToStr(params));
+
+        System.out.println("accessToken:"+accessToken);
+
+        //使用 accessToken 请求受保护资源服务
+
+        Map<String, String> paramsMap = new HashMap<String, String>();
+
+        paramsMap.put("app_id","APPID_RABBIT");
+        paramsMap.put("app_secret","APPSECRET_RABBIT");
+        paramsMap.put("token",accessToken);
+
+        String result = HttpURLClient.doPost(protectedURl,HttpURLClient.mapToStr(paramsMap));
+        System.out.println("accessToken:" + result);
+
+    }
+}
---
+++ demos/oauth2-code-master/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wangxindong
+  Date: 2020/2/16
+  Time: 上午11:27
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>OAuth 2 code test</title>
+  </head>
+  <body>
+  OAuth 2 code test
+  please go to ch03 or ch09
+  </body>
+</html>
